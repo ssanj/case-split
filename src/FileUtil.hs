006924a839b324cf85b9f41108c05c4ec293a9d1
sanjiv sahayam <sanjsmailbox@gmail.com>
2018-04-01 00:45:22 +1100

    add typesafe results to ADT matches

--- src/FileUtil.hs
+++ src/FileUtil.hs
@@ -7,11 +7,16 @@ module FileUtil (getScalaFilesInDir,
 
 import Data.Char (isSpace)
 import Data.Maybe (isJust)
+import Control.Applicative (liftA2)
 import Control.Monad (filterM)
 import Data.List (dropWhileEnd, find, isSuffixOf, isPrefixOf)
 import System.Directory
 import System.FilePath.Posix
 import AdtParser
+
+data AdtMatchedResults     = AdtMatchedResults [FilePath] deriving Show
+data AdtConsideredResults = AdtConsideredResults [FilePath] deriving Show
+
 
 trim :: String -> String
 trim = dropWhileEnd isSpace . dropWhile isSpace
@@ -31,17 +36,19 @@ getDirectories root = do files <- listDirectory root
                          _ <- mapM_ putStrLn absoluteFiles
                          filterM doesDirectoryExist absoluteFiles
 
-
+-- TODO: Handle directory read errors - skip and continue
 getAllSubDirectories :: FilePath -> IO [FilePath]
 getAllSubDirectories root = do dirs <- getDirectories root
                                let subs    = fmap getAllSubDirectories dirs
                                    allDirs = (pure dirs) : subs
                                foldMap id allDirs
 
-findAdtMatches :: FilePath -> IO [FilePath]
+findAdtMatches :: FilePath -> IO (AdtMatchedResults, AdtConsideredResults)
 findAdtMatches root = do allDirs <- getAllSubDirectories root
                          let scalaFiles = fmap getScalaFilesInDir allDirs -- [IO [FilePath]]
-                         foldMap (>>= findFilesThatMatch) scalaFiles
+                         (matched, considered) <- foldMap (fmap (\fp -> (findFilesThatMatch fp, pure fp))) scalaFiles -- (IO [FilePath], IO [FilePath])
+                         liftA2 (\m c -> (AdtMatchedResults m, AdtConsideredResults c)) matched considered
+
 
 findFilesThatMatch :: [FilePath] -> IO [FilePath]
 findFilesThatMatch files = filterM (\f -> isJust . find hasADT <$> (fetchLines f)) files
